e38f883cb832bd2cfd1397222c8f5540200b185a
Anish Tendolkar <anish.tendolkar@yahoo.com>
2026-08-30 20:48:32 -0500

    images in academic section updated

--- src/sections/AcademicProjects.jsx
+++ src/sections/AcademicProjects.jsx
@@ -2,12 +2,42 @@ import { motion } from "framer-motion"
 import { Link } from "react-router-dom"
 
 import performingArts from "../assets/projects/academic/performing-arts/Picture1.png"
+import breakingBarriers from "../assets/projects/academic/breaking-barriers/Picture4.png"
+import bombayZoo from "../assets/projects/academic/bombay-zoo/image (2).png"
+import arcadia from "../assets/projects/academic/arcadia/image (2).png"
+import srmCampus from "../assets/projects/academic/srm-campus/image (2).png"
+import rethinkingRehab from "../assets/projects/academic/rethinking-rehab/image (2).png"
 
 const projects = [
   {
     title: "Institute of Performing Arts",
     image: performingArts,
     path: "/projects/performing-arts",
+  },
+  {
+    title: "Breaking Barriers",
+    image: breakingBarriers,
+    path: "/projects/breaking-barriers",
+  },
+  {
+    title: "Bombay Zoo",
+    image: bombayZoo,
+    path: "/projects/bombay-zoo",
+  },
+  {
+    title: "The Arcadia",
+    image: arcadia,
+    path: "/projects/arcadia",
+  },
+  {
+    title: "SRM Campus",
+    image: srmCampus,
+    path: "/projects/srm-campus",
+  },
+  {
+    title: "Rethinking Rehab",
+    image: rethinkingRehab,
+    path: "/projects/rethinking-rehab",
   },
 ]
 
@@ -17,7 +47,7 @@ function AcademicProjects() {
       id="academic"
       className="px-6 md:px-12 lg:px-20 py-32 bg-[#EFEAE3]"
     >
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-6xl mx-auto">
 
         <motion.div
           initial={{ opacity: 0, y: 50 }}
@@ -42,7 +72,7 @@ function AcademicProjects() {
               to={project.path}
               className="group block"
             >
-              <div className="relative overflow-hidden rounded-[2rem] aspect-[4/3]">
+              <div className="relative overflow-hidden rounded-[1.5rem] aspect-[4/3]">
                 <img
                   src={project.image}
                   alt={project.title}
@@ -51,8 +81,8 @@ function AcademicProjects() {
 
                 <div className="absolute inset-0 bg-black/10 group-hover:bg-black/35 transition-colors duration-500" />
 
-                <div className="absolute bottom-0 left-0 p-8">
-                  <h3 className="font-serif text-3xl md:text-4xl text-white">
+                <div className="absolute bottom-0 left-0 p-6">
+                  <h3 className="font-serif text-2xl md:text-3xl text-white">
                     {project.title}
                   </h3>
                 </div>
